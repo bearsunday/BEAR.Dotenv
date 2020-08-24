1724b245d2e542e8eba018480dc6420c140bcd51
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-08-24 18:38:39 +0900

    Remove Exception

--- src/Exception/LogicException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace BEAR\Dotenv\Exception;
-
-class LogicException extends \LogicException
-{
-}
--- src/Exception/RuntimeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace BEAR\Dotenv\Exception;
-
-class RuntimeException extends \RuntimeException
-{
-}

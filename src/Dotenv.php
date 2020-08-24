d75978bd25813404cdb94fefccd54faa75bb60f3
Akihito Koriyama <akihito.koriyama@gmail.com>
2020-08-24 18:38:28 +0900

    Create Dotenv class

--- src/Dotenv.php
+++ src/Dotenv.php
@@ -4,6 +4,26 @@ declare(strict_types=1);
 
 namespace BEAR\Dotenv;
 
-class Dotenv
+use Dotenv\Dotenv as PhpDotenv;
+
+use function file_exists;
+use function realpath;
+use function sprintf;
+
+final class Dotenv
 {
+    public function load(string $dir): void
+    {
+        if (file_exists((string) realpath(sprintf('%s/.env', $dir)))) {
+            PhpDotenv::createImmutable($dir)->load();
+
+            return;
+        }
+
+        if (! file_exists((string) realpath(sprintf('%s/.env.dist', $dir)))) {
+            return;
+        }
+
+        (PhpDotenv::createImmutable($dir, '.env.dist'))->load();
+    }
 }
--- tests/DotenvTest.php
+++ tests/DotenvTest.php
@@ -8,19 +8,28 @@ use PHPUnit\Framework\TestCase;
 
 class DotenvTest extends TestCase
 {
-    /**
-     * @var Dotenv
-     */
-    protected $dotenv;
+    protected Dotenv $dotenv;
 
-    protected function setUp() : void
+    protected function setUp(): void
     {
-        $this->dotenv = new Dotenv;
+        $this->dotenv = new Dotenv();
     }
 
-    public function testIsInstanceOfDotenv() : void
+    public function testIsInstanceOfDotenv(): void
     {
         $actual = $this->dotenv;
         $this->assertInstanceOf(Dotenv::class, $actual);
+    }
+
+    public function testLoadDist(): void
+    {
+        $this->dotenv->load(__DIR__ . '/Fake/dist');
+        $this->assertSame($_ENV['FOO'], 'BAR');
+    }
+
+    public function testLoadEnv(): void
+    {
+        $this->dotenv->load(__DIR__ . '/Fake/env');
+        $this->assertSame($_ENV['BAR'], 'BAZ');
     }
 }
